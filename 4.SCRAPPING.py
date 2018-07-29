0d178934c347cb77135c435c975949a87fc73376
yashk2000 <41234408+yashk2000@users.noreply.github.com>
2018-07-29 22:36:18 +0530

    Update 4.SCRAPPING.py

--- 4.SCRAPPING.py
+++ 4.SCRAPPING.py
@@ -1,12 +1,12 @@
 import webbrowser
 import requests
 import bs4#contians eautiful soup4
-def scrapper():
-	str=raw_input("enter search parameter:")
-	res=requests.get('https://google.com/search?q='+str)
-	soup=bs4.BeautifulSoup(res.text,'lxml')
-	links=soup.select('.r a')#selector
-	results=min(10, len(links))#searches for 10 links
-	for i in range(results):
-		   webbrowser.open('https://google.com' + links[i].get('href'))
-scrapper()
+
+str=raw_input("enter search parameter:")
+res=requests.get('https://google.com/search?q='+str)
+soup=bs4.BeautifulSoup(res.text,'lxml')
+links=soup.select('.r a')#selector
+results=min(10, len(links))#searches for 10 links
+for i in range(results):
+	   webbrowser.open('https://google.com' + links[i].get('href'))
+
