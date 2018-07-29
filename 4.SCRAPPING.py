570ab8c050d237a060f252df2ed424d6ccbbdf32
yashk2000 <41234408+yashk2000@users.noreply.github.com>
2018-07-29 22:21:14 +0530

    Update 4.SCRAPPING.py

--- 4.SCRAPPING.py
+++ 4.SCRAPPING.py
@@ -4,7 +4,6 @@ import requests
 import bs4#contians eautiful soup4
 def scrapper():
 	str=raw_input("enter search parameter:")
-	keyword=str.join(sys.args[1:])
 	res=requests.get('https://google.com/search?q='+str)
 	soup=bs4.BeautifulSoup(res.text,'lxml')
 	links=soup.select('.r a')#selector
