d93b2223c7070603537578e5275b0a95f0ce6275
yashk2000 <41234408+yashk2000@users.noreply.github.com>
2018-07-30 05:58:49 +0530

    Update 6.CAPCHA_READING.py

--- 6.CAPCHA_READING.py
+++ 6.CAPCHA_READING.py
@@ -2,11 +2,11 @@ import cv2
 import pytesseract
 from PIL import Image
 
-path=raw_input("enter file name:")
+path=raw_input("enter file address:")
 image=cv2.imread(path)
 gray=cv2.wtcolor(image,cv2.COLOR_BGR2GRAY)
-filename="image.png"
-cv.imwrite(filename, gray)
+filename=raw_input("enter where to store greyscale image:")
+cv.imwrite(filename, gray)  
 text=pytesseract.image_to_string(Image.open(filename))
 text1=""
 text2=""
