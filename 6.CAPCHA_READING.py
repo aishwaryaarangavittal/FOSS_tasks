33b4cfb8fc778204114bff7ae6a2004f69a08cb7
yashk2000 <41234408+yashk2000@users.noreply.github.com>
2018-07-30 05:38:59 +0530

    Update 6.CAPCHA_READING.py

--- 6.CAPCHA_READING.py
+++ 6.CAPCHA_READING.py
@@ -1,20 +1,20 @@
-import cv2
+mport cv2
 import pytesseract
 from PIL import Image
 
-path=raw_input("enter file name:").strip()
+path=raw_input("enter file name:")
 image=cv2.imread(path)
 gray=cv2.wtcolor(image,cv2.COLOR_BGR2GRAY)
-filename="{}.png".format("temp")
+filename="image.png"
 cv.imwrite(filename, gray)
 text=pytesseract.image_to_string(Image.open(filename))
 text1=""
 text2=""
-for letter in text
-    if text[letter]=='+'
+for i in len(text):
+    if text[letter]=='+':
      text1=text[0,letter]
      text2=text[letter+1:]
-i=(int)text1
-j=(int)text2
+i=int(text1,[10])
+j=int(text2,[10])
 k=i+j
 print k
