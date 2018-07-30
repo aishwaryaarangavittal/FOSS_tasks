501a7f09e3cf3bc383d9ad665d5fc6fc0ad60483
yashk2000 <41234408+yashk2000@users.noreply.github.com>
2018-07-30 12:45:32 +0530

    Update 4.SCRAPPING.py

--- 4.SCRAPPING.py
+++ 4.SCRAPPING.py
@@ -6,7 +6,7 @@ str=raw_input("enter search parameter:")
 res=requests.get('https://google.com/search?q='+str)
 soup=bs4.BeautifulSoup(res.text,'lxml')
 links=soup.select('.r a')#selector
-results=min(10, len(links))#searches for 10 links
+results=min(10, len(links))
 for i in range(results):
 	   webbrowser.open('https://google.com' + links[i].get('href'))
 
